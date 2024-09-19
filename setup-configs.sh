ddae7b1740e19a48f55654056caacec23acf2d2c
Petteri Mattila <petterimattila@protonmail.com>
2024-09-19 13:43:43 +0300

    Fixing script

--- setup-configs.sh
+++ setup-configs.sh
@@ -5,8 +5,20 @@ hyprland_config_dir="$HOME/.config/hypr"
 waybar_config_dir="$HOME/.config/waybar"
 mako_config_dir="$HOME/.config/mako"
 
-waybar_config_path="$waybar_config_dir/config"
+waybar_config_path="$waybar_config_dir/"
 mako_config_path="$mako_config_dir/config"
+
+# Remove any existing config file or link
+if [ -L "$waybar_config_path" ] || [ -e "$waybar_config_path" ]; then
+    echo "Removing existing Waybar configuration file..."
+    rm -f "$waybar_config_path/config.jsonc"
+    rm -f "$waybar_config_path/style.css"
+fi
+
+if [ -L "$mako_config_path" ] || [ -e "$mako_config_path" ]; then
+    echo "Removing existing Mako configuration file..."
+    rm -f "$mako_config_path"
+fi
 
 # Check if configuration directory exists, create if not
 if [ ! -d "$mako_config_dir" ]; then
@@ -19,19 +31,9 @@ if [ ! -d "$waybar_config_dir" ]; then
     mkdir -p "$waybar_config_dir"
 fi
 
-# Remove any existing config file or link
-if [ -L "$waybar_config_path" ] || [ -e "$waybar_config_path" ]; then
-    echo "Removing existing Waybar configuration file..."
-    rm -f "$waybar_config_path"
-fi
-
-if [ -L "$mako_config_path" ] || [ -e "$mako_config_path" ]; then
-    echo "Removing existing Mako configuration file..."
-    rm -f "$mako_config_path"
-fi
-
 # Create a symbolic link
-ln -s "$hyprland_config_dir/waybar/config" "$waybar_config_path"
+ln -s "$hyprland_config_dir/waybar/config.jsonc" "$waybar_config_path/config.jsonc"
+ln -s "$hyprland_config_dir/waybar/style.css" "$waybar_config_path/style.css"
 ln -s "$hyprland_config_dir/mako/config" "$mako_config_path"
 
 echo "Configurations are successfully linked."
