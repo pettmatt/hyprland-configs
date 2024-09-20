98b39e6abc9ec91af92e86909ee851a79a38fc08
Petteri Mattila <petterimattila@protonmail.com>
2024-09-20 14:57:15 +0300

    swww-random script

--- scripts/swww-random.sh
+++ scripts/swww-random.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
+# Original: https://wiki.archlinux.org/title/Hyprland
 # This script will randomly go through the files of a directory, setting it
 # up as the wallpaper at regular intervals
-#
 # NOTE: this script uses bash (not POSIX shell) for the RANDOM variable
 
 if [[ $# -lt 1 ]] || [[ ! -d $1   ]]; then
