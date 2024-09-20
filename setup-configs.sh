d650f16a4ced238b7d27e22dbacfeece4b3d1143
Petteri Mattila <petterimattila@protonmail.com>
2024-09-20 15:06:28 +0300

    wofi base added

--- setup-configs.sh
+++ setup-configs.sh
@@ -4,9 +4,11 @@
 hyprland_config_dir="$HOME/.config/hypr"
 waybar_config_dir="$HOME/.config/waybar"
 mako_config_dir="$HOME/.config/mako"
+wofi_config_dir="$HOME/.config/wofi"
 
 waybar_config_path="$waybar_config_dir/"
 mako_config_path="$mako_config_dir/config"
+wofi_config_path="$wofi_config_dir"
 
 # Remove any existing config file or link
 if [ -L "$waybar_config_path" ] || [ -e "$waybar_config_path" ]; then
@@ -21,16 +23,27 @@ if [ -L "$mako_config_path" ] || [ -e "$mako_config_path" ]; then
     rm -f "$mako_config_path"
 fi
 
+if [ -L "$wofi_config_path" ] || [ -e "$wofi_config_path" ]; then
+    echo "Removing existing Wofi configuration file..."
+    rm -f "$wofi_config_path"
+fi
+
 # Check if configuration directory exists, create if not
+if [ ! -d "$waybar_config_dir" ]; then
+    echo "Creating Waybar configuration directory..."
+    mkdir -p "$waybar_config_dir"
+fi
+
 if [ ! -d "$mako_config_dir" ]; then
     echo "Creating Mako configuration directory..."
     mkdir -p "$mako_config_dir"
 fi
 
-if [ ! -d "$waybar_config_dir" ]; then
-    echo "Creating Waybar configuration directory..."
-    mkdir -p "$waybar_config_dir"
+if [ ! -d "$wofi_config_dir" ]; then
+    echo "Creating Wofi configuration directory..."
+    mkdir -p "$wofi_config_dir"
 fi
+
 
 # Create a symbolic link
 # Waybar
@@ -39,5 +52,7 @@ ln -s "$hyprland_config_dir/waybar/style.css" "$waybar_config_path/style.css"
 ln -s "$hyprland_config_dir/waybar/theme.css" "$waybar_config_path/theme.css"
 # Mako
 ln -s "$hyprland_config_dir/mako/config" "$mako_config_path"
+# Wofi
+ln -s "$hyprland_config_dir/wofi/style.css" "$wofi_config_path/style.css"
 
 echo "Configurations are successfully linked."
